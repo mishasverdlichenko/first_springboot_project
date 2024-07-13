141d5906a3348b0765cf018778523cee098cb263
mishasverdlichenko <friendly121199@gmail.com>
2024-07-13 12:59:31 +0300

    first spring boot app

---
+++ src/main/java/com/example/first_sprintboot_project/GreetingController.java
@@ -0,0 +1,4 @@
+package com.example.first_sprintboot_project;
+
+public class GreetingController {
+}
---
+++ src/test/java/com/example/first_sprintboot_project/FirstSprintbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_sprintboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSprintbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
