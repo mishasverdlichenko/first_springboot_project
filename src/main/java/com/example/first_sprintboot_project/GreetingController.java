c14bfeb5d49c6cdcd53edfc66faf13bea2594a3c
mishasverdlichenko <friendly121199@gmail.com>
2024-07-13 13:02:28 +0300

    Update GreetingController.java

--- src/main/java/com/example/first_sprintboot_project/GreetingController.java
+++ src/main/java/com/example/first_sprintboot_project/GreetingController.java
@@ -1,4 +1,13 @@
 package com.example.first_sprintboot_project;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GreetingController {
+  @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(String.format("Hello, %s", name));
+    }
 }
