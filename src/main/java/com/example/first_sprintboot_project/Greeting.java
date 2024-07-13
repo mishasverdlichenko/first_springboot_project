214276160ffff5d52b4567f07844abae2a1d887e
mishasverdlichenko <friendly121199@gmail.com>
2024-07-13 13:02:46 +0300

    Update Greeting.java

--- src/main/java/com/example/first_sprintboot_project/Greeting.java
+++ src/main/java/com/example/first_sprintboot_project/Greeting.java
@@ -1,4 +1,12 @@
 package com.example.first_sprintboot_project;
 
 public class Greeting {
+   private final String content;
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+    public String getContent() {
+        return content;
+    }
 }
